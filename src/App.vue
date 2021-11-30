2b598e33085c01f4ce34e2e76396fda399e47353
BobbyLilD <steblin73@gmail.com>
2021-11-30 21:14:57 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <NavBar />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue"
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    NavBar
   },
 };
 </script>
@@ -24,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img{
+  max-width: 400px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="nav">
+        <router-link class="nav-link">
+            <button class="nav-btn">Feed</button>
+        </router-link>
+
+        <router-link class="nav-link">
+            <button class="nav-btn">Requests</button>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style>
+    #nav{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        margin: 16px 0px;
+    }
+    .nav-btn{
+        flex-grow: 1;
+        padding: 12px 24px;
+    }
+</style>
